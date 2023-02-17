42044615f85846ae023f727921bf8af5d44cc407
ftp <altovovu_api@oscar1.beget.ru>
2023-02-17 14:30:57 +0300

    first commit

---
+++ config/auth.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+    ],
+];
